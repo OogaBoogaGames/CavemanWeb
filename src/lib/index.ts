2a2093d378cb15420797061a93f3d0704e58026b
endercass <46408156+Endercass@users.noreply.github.com>
2023-12-24 21:29:47 -0500

    Migrate caveman components to library

---
+++ src/lib/index.ts
@@ -0,0 +1,4 @@
+import Caveman from "$components/Caveman.svelte";
+import CavemanImage from "$components/CavemanImage.svelte";
+
+export { Caveman, CavemanImage };
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Caveman from "$components/Caveman.svelte";
+  import CavemanImage from "$components/CavemanImage.svelte";
+  let packages: string[] = ["games.oogabooga.demobundle.cvmn"];
+</script>
+
+<h1>Welcome to your library project</h1>
+<p>
+  Create your package using @sveltejs/package and preview/showcase your work
+  with SvelteKit
+</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<Caveman {packages} mirror={window.location.href}>
+  <CavemanImage
+    packageid="games.oogabooga.demobundle.cvmn"
+    assetid="characters.tux"
+    alt="Caveman"
+    width="400"
+    height="400"
+  />
+</Caveman>
